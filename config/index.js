a29b60d4571d517c22beaa458cc4963f44e8cdf6
wumaozhang <112810779+wumaozhang@users.noreply.github.com>
2022-09-04 21:48:08 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,18 +11,8 @@ export const config = {
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
-   * [
-   *  {
-   *    // 想要发送的人的名字
-   *    name: "老婆0",
-   *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
-   *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
-   *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-   *    openUrl: "https://wangxinleo.cn"
-   *   }, 
-   * ]
+   * [oZPn456WzJwCDb81ovHWxPmrad6A
+   *  
    */
   USERS: [
     {
@@ -36,27 +26,10 @@ export const config = {
       province: "天津",
       // 所在城市
       city: "蓟州区",
-      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      //openUrl: "https://wangxinleo.cn"
+      
     }, 
     
-    {
-      name: "老婆2",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      openUrl: "https://wangxinleo.cn"
-    }, 
-    {
-      name: "老婆3", 
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      openUrl: "https://wangxinleo.cn"
-    }, 
-  ],
+    
 
   /**
    * 回调消息 相关，主要用来展示发送是否成功/失败的数据
@@ -82,9 +55,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "天津",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "蓟州区",
 
   /** 重要节日相关 */
 
